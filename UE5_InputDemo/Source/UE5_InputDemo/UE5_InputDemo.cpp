3378c3d69ad053c4e9c79fcea18a1e6a778e72a5
James (Jay) Vickers <jayvickers5@googlemail.com>
2023-11-10 14:40:54 +0000

    removed project files
    Just the needed files have been kept

--- UE5_InputDemo/Source/UE5_InputDemo/UE5_InputDemo.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE5_InputDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_InputDemo, "UE5_InputDemo" );
- 
